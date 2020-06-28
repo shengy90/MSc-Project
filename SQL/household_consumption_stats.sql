52fdc05323507efa676a6ba9c1094e891f1a14b9
Shengy <shengy90@gmail.com>
2020-06-28 17:36:39 +0100

    include dtou users

--- SQL/household_consumption_stats.sql
+++ SQL/household_consumption_stats.sql
@@ -10,9 +10,22 @@ CAST(TIMESTAMP_TRUNC(ts, DAY) AS DATE) AS day,
 EXTRACT(HOUR FROM ts) AS hour,
 EXTRACT(MINUTE FROM ts) AS minute,
 
+COALESCE(tariffs.Tariff, "Normal") AS tariff_type,
+
+CASE 
+    WHEN stdortou = 'Tou' AND tariffs.Tariff = "Low" THEN 3.99
+    WHEN stdortou = 'Tou' AND  tariffs.Tariff = "Normal" THEN 11.76
+    WHEN stdortou = 'Tou' AND  tariffs.Tariff = "High" THEN 67.20
+    ELSE 14.228 
+    END AS tariff,
 SAFE_CAST(kwhh AS FLOAT64) AS kwhh,
-REPLACE(acorn, "ACORN-A", "") AS acorn,
+REPLACE(acorn, "ACORN-", "") AS acorn,
 acorn_grouped,
+
 MOD(ABS(FARM_FINGERPRINT(lcl_id)),100) AS sample
-FROM `machine-learning-msc.low_carbon_london.household_consumption_raw_stats` 
-WHERE stdortou = 'Std'
+
+FROM `machine-learning-msc.low_carbon_london.household_consumption_raw_stats` raw_stats
+LEFT JOIN `machine-learning-msc.low_carbon_london.dtou_tariffs` tariffs ON tariffs.TariffDateTime = raw_stats.ts
+
+
+
