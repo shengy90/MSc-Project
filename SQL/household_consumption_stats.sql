a2434b7b57029f3bcce2067a4bb269b8b4ad2251
Shengy <shengy90@gmail.com>
2020-07-05 17:22:03 +0100

    add hhourly and hourly ranks

--- SQL/household_consumption_stats.sql
+++ SQL/household_consumption_stats.sql
@@ -7,8 +7,10 @@ CAST(TIMESTAMP_TRUNC(ts, YEAR) AS DATE) AS year,
 CAST(TIMESTAMP_TRUNC(ts, MONTH) AS DATE) AS month,
 CAST(TIMESTAMP_TRUNC(ts, DAY) AS DATE) AS day,
 
-EXTRACT(HOUR FROM ts) AS hour,
-EXTRACT(MINUTE FROM ts) AS minute,
+(IF(EXTRACT(MINUTE FROM ts)=0, EXTRACT(HOUR FROM ts)*2-1, EXTRACT(HOUR FROM ts)*2)+1) AS hhourly_rank, -- half-hourly rank, starts from 0000=1 to 2330 = 47
+EXTRACT(HOUR FROM ts) AS hourly_rank, -- hourly rank,
+EXTRACT(DAYOFWEEK FROM ts) AS dayofweek, -- 1 = sunday, 7 = monday 
+EXTRACT(DAY FROM ts) AS dayofmonth, 
 
 stdortou,
 COALESCE(tariffs.Tariff, "Normal") AS tariff_type,
@@ -22,10 +24,12 @@ SAFE_CAST(kwhh AS FLOAT64) AS kwhh,
 REPLACE(acorn, "ACORN-", "") AS acorn,
 acorn_grouped,
 
+
 MOD(ABS(FARM_FINGERPRINT(lcl_id)),100) AS sample
 
 FROM `machine-learning-msc.low_carbon_london.household_consumption_raw_stats` raw_stats
 LEFT JOIN `machine-learning-msc.low_carbon_london.dtou_tariffs` tariffs ON tariffs.TariffDateTime = raw_stats.ts
+
 
 
 
